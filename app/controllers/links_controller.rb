e9e7b9343d74f7e006860c9d8acb0c230f151834
jazzthief <mynameisyegor@gmail.com>
2020-10-11 00:27:50 +0300

    Add error handling and message

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,6 +4,12 @@ class LinksController < ApplicationController
         # js endpoint
         shortener = Shortener.new(link_params[:original_url])
         @link = shortener.generate_short_link
+
+        if @link.persisted?
+            respond_to :js
+        else
+            render 'error.js.erb'
+        end
     end
 
     private
